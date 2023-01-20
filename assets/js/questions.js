001e8b5e1bc1d8e309d4f729d31927c4997f0459
digitinok <75326757+digitinok@users.noreply.github.com>
2023-01-20 20:05:53 +0000

    add question object

---
+++ assets/js/questions.js
@@ -0,0 +1,13 @@
+//array of questions
+let questions = [
+
+    {title: "question1",
+    choices: ["a","b","c","d"],
+    answer: "b",
+    },
+    {title: "question2",
+    choices: ["a","b","c","d"],
+    answer: "d",
+    },
+]
+console.log(questions)
